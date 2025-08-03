6af425da0222516151177b236f03f52cd89f5799
mac1kus <mac1kus@gmail.com>
2025-08-03 16:42:15 +0530

    updated app

--- app.py
+++ app.py
@@ -1244,6 +1244,8 @@ def download_file(filename):
 
 # Main application entry point
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
             
     
